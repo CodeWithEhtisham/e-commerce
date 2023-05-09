dfa435504e45f9aa0d793695f7811de361a10a32
CodeWithEhtisham <shamikhan0347@gmail.com>
2023-05-09 16:41:01 +0500

    admin panel template ready

--- apps/adminpanel/urls.py
+++ apps/adminpanel/urls.py
@@ -1,5 +1,12 @@
 
 from django.urls import path
-
+from . import views
 urlpatterns = [
+    path('admin/', views.admin_index, name='admin_index'),
+    path('admin/orders', views.orders, name='orders'),
+    path('admin/add_product', views.add_product, name='add_product'),
+    path('admin/category', views.category, name='category'),
+    path('admin/invoice', views.invoice, name='invoice'),
+    path('admin/view_product', views.view_product, name='view_product'),
+    path('admin/view_user', views.view_user, name='view_user'),
 ]
--- apps/adminpanel/views.py
+++ apps/adminpanel/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+def admin_index(request):
+    return render(request, 'admin_index.html')
+
+def orders(request):
+    return render(request, 'orders.html')
+
+def add_product(request):
+    return render(request, 'add_product.html')
+
+def category(request):
+    return render(request, 'category.html')
+
+def invoice(request):
+    return render(request, 'invoice.html')
+
+def view_product(request):
+    return render(request, 'view_product.html')
+
+def view_user(request):
+    return render(request, 'view_user.html')
+

893da7a8d2b5e68b09f6ccbfe03d79d90e67d2ec
CodeWithEhtisham <shamikhan0347@gmail.com>
2023-05-10 15:45:31 +0500

    added

--- apps/website/urls.py
+++ apps/website/urls.py
@@ -5,5 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login, name='login'),
-    path('register', views.register, name='register')
+    path('register', views.register, name='register'),
+    path('shop_grid_fullwidth', views.shop_grid_fullwidth, name='shop_grid_fullwidth'),
+    path('cart', views.cart, name='cart'),
+    path('checkout', views.checkout, name='checkout'),
+    path('checkout_success', views.checkout_success, name='checkout_success'),
+    path('collections', views.collections, name='collections'),
+    path('product_details', views.product_details, name='product_details'),
 ]
--- apps/website/views.py
+++ apps/website/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import CustomUser
 
 # Create your views here.
 def index(request):
@@ -9,5 +10,40 @@ def login(request):
     return render(request, 'login.html')
 
 def register(request):
+    if request.method == 'POST':
+        if CustomUser.objects.filter(email=request.POST['email']).exists():
+            return render(request, 'register.html', {'error': 'Email already exists'})
+        first_name = request.POST['first-name']
+        last_name = request.POST['last-name']
+        email = request.POST['email']
+        password = request.POST['password']
+        contact= request.POST['contact']
+        address = request.POST['address']
+        try:
+            CustomUser.objects.create_user(first_name=first_name, last_name=last_name, email=email, password=password, contact=contact, address=address)
+            return render(request, 'login.html', {'success': 'User created successfully'})
+        except Exception as e:
+            print(e)
+            return render(request, 'register.html', {'error': 'Something went wrong'})
+
     return render(request, 'register.html')
 
+
+def shop_grid_fullwidth(request):
+    return render(request, 'shop_grid_fullwidth.html')
+
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def checkout_success(request):
+    return render(request, 'checkout-success.html')
+
+def collections(request):
+    return render(request, 'collections.html')
+
+def product_details(request):
+    return render(request, 'product-details.html')
